f6cbd97bee14dafb4a6eed52ee7bb35d90d1f459
Hadji Gizel <gizel.hadji@etu.univ-orleans.fr>
2018-12-04 09:30:56 +0100

    vue QQ

--- src/vue/VueObjets.java
+++ src/vue/VueObjets.java
@@ -13,8 +13,8 @@ import java.io.FileNotFoundException;
  * Ta doc ici
  */
 public enum VueObjets implements IObjet{
-    CAISSE('$', "CAISSE.PNG"),
-    JOUEUR('@', "JOUEUR.PNG"),
+    CAISSE('$', "32j.gif"),
+    JOUEUR('@', "48j.png"),
     MUR('#', "MUR.PNG"),
     SOL(' ',"SOL.PNG"),
     TROU('.',"TROU.PNG"),
@@ -24,10 +24,11 @@ public enum VueObjets implements IObjet{
     private char etat;
     private Image sprite;
 
+
     VueObjets(char etat, String Ssprite) {
         this.etat = etat;
         try {
-            this.sprite = new Image(new FileInputStream(Ssprite),80,80,false,false);
+            this.sprite = new Image(new FileInputStream(Ssprite),48,48,false,false);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
