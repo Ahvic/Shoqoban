44f8303a017a667e28dbc4afb72d1cc82e47fb38
Fakehex <gizel.hadji@etu.univ-orleans.fr>
2018-12-14 18:36:02 +0100

    VueNbCoup

---
+++ src/vue/VueNbCoup.java
@@ -0,0 +1,14 @@
+package vue;
+
+import controller.Controleur;
+
+import javafx.scene.control.Label;
+
+public class VueNbCoup extends Vue{
+
+    Label label = new Label("0");
+
+    public void dessine() {
+        label.setText(Controleur.getInstance().getNbCoup()+"");
+    }
+}
