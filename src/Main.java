5c1103a7fea1b0484cb66d398eda4bb570c7b252
calgaryz <frederik.voigt@etu.univ-orleans.fr>
2018-12-15 21:59:24 +0100

    modifs

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
-
+import vue.IHMFX;
 
 public class Main {
     public static void main(String[] args) {
         Thread thread = new Thread() {
             public void run() {
-                new IHMFX.lance();
+                IHMFX i = new IHMFX();
+                i.lance();
             }
         };
 
